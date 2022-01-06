b8705e009e772109b318ae59ee77d3fff5fa0416
xor <mrkeule@gmail.cpom>
2022-01-06 17:18:23 +0100

    new attempt

--- maf/examples/MNISTRun.py
+++ maf/examples/MNISTRun.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     cache_dir = StaticMethods.cache_dir()
     checkpoints_dir = Path(cache_dir, "mnist_checkpoints")
     checkpoints_dir.mkdir(exist_ok=True)
-    prefix = "mnist_12_[5]2"
+    prefix = "mnist_30"
     result_dir: Path = Path("results_mnist")
     Global.set_global('results_dir', result_dir)
     mist = Mist(conditional=False,
