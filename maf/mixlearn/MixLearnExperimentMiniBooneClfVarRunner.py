3072829f5b93a9d5572a94473e927c9fd649ae4c
xor <mrkeule@gmail.cpom>
2022-02-03 00:35:33 +0100

    miniboone test

--- maf/mixlearn/MixLearnExperimentMiniBooneClfVarRunner.py
+++ maf/mixlearn/MixLearnExperimentMiniBooneClfVarRunner.py
@@ -1,3 +1,4 @@
+from distributions.base import enable_memory_growth
 from typing import Type
 
 from common.globals import Global
@@ -15,10 +16,33 @@ class MixLearnExperimentMiniBooneClfVarRunner(MafExperiment):
         pass
 
     def _run(self):
+        # creator = MAFCreator(batch_norm=True,
+        #                      conditional_one_hot=False,
+        #                      # epochs=epochs,
+        #                      hidden_shape=[100, 100],
+        #                      input_noise_variance=0.0,
+        #                      layers=20,
+        #                      norm_layer=False,
+        #                      use_tanh_made=True)
+        # results_folder = Global.get_default('results_dir', Path('results_miniboone'))
+        # experiment = MixLearnExperimentMiniBooneClfVar(name=self.name,
+        #                                                learned_distr_creator=creator,
+        #                                                dataset_name='miniboone',
+        #                                                result_folder=results_folder,
+        #                                                experiment_init_ds_class=self.experiment_init_ds_class,
+        #                                                paper_load=False,
+        #                                                clf_epochs=1000,
+        #                                                clf_patience=20,
+        #                                                nf_epochs=2000,
+        #                                                nf_patience=20,
+        #                                                batch_size=1024,
+        #                                                classifiers_per_nf=1,
+        #                                                just_signal_plan=False)
+        #
         creator = MAFCreator(batch_norm=True,
                              conditional_one_hot=False,
                              # epochs=epochs,
-                             hidden_shape=[1024, 1024],
+                             hidden_shape=[200, 200],
                              input_noise_variance=0.0,
                              layers=30,
                              norm_layer=False,
@@ -32,11 +56,12 @@ class MixLearnExperimentMiniBooneClfVarRunner(MafExperiment):
                                                        paper_load=False,
                                                        clf_epochs=1000,
                                                        clf_patience=20,
-                                                       nf_epochs=4000,
-                                                       nf_patience=100,
+                                                       nf_epochs=2000,
+                                                       nf_patience=20,
                                                        batch_size=1024,
                                                        classifiers_per_nf=1,
                                                        just_signal_plan=False)
+
         experiment.create_training_plan().run()
 
     def __init__(self, name='miniboone_clfvar'):
