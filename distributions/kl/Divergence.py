fix: kl, cache printer
JS still broken

distributions/kl/Divergence.py
@@ -30,7 +30,7 @@ class Divergence(Ser):
         self.log_epsilon = tf.cast(tf.math.log(K.epsilon()), dtype=tf.float32)
         self.epsilon = tf.cast(K.epsilon(), dtype=tf.float32)
 
-    def calculate_from_samples_vs_p(self, ds_q_samples: DS, log_q_samples: DS) -> float:
+    def calculate_from_samples_vs_q(self, ds_p_samples: DS, log_p_samples: DS) -> float:
         raise NotImplementedError()
 
     def calculate_by_sampling_space(self) -> float:

maf/stuff/DivergenceExperiment.py
@@ -142,14 +142,16 @@ class DivergenceExperiment(MafExperiment):
             return
         values = []
         for maf in self.mafs:
-            j = JensenShannonDivergence(p=maf, q=self.data_distribution, half_width=self.divergence_half_width, step_size=self.divergence_step_size, batch_size=self.batch_size)
-            k = KullbackLeiblerDivergence(p=maf, q=self.data_distribution, half_width=self.divergence_half_width, step_size=self.divergence_step_size, batch_size=self.batch_size)
+            # j = JensenShannonDivergence(p=maf, q=self.data_distribution, half_width=self.divergence_half_width, step_size=self.divergence_step_size, batch_size=self.batch_size)
+            # k = KullbackLeiblerDivergence(p=maf, q=self.data_distribution, half_width=self.divergence_half_width, step_size=self.divergence_step_size, batch_size=self.batch_size)
+            j = JensenShannonDivergence(q=maf, p=self.data_distribution, half_width=self.divergence_half_width, step_size=self.divergence_step_size, batch_size=self.batch_size)
+            k = KullbackLeiblerDivergence(q=maf, p=self.data_distribution, half_width=self.divergence_half_width, step_size=self.divergence_step_size, batch_size=self.batch_size)
             if self.ds_samples is None:
                 jsd = j.calculate_by_sampling_p(self.divergence_sample_size) if self.divergence_sample_size is not None else j.calculate_by_sampling_space()
                 kld = k.calculate_by_sampling_p(self.divergence_sample_size) if self.divergence_sample_size is not None else k.calculate_by_sampling_space()
             else:
-                jsd = j.calculate_from_samples_vs_p(ds_q_samples=self.ds_samples,log_q_samples=self.log_ps_samples)
-                kld = k.calculate_from_samples_vs_p(ds_q_samples=self.ds_samples,log_q_samples=self.log_ps_samples)
+                jsd = j.calculate_from_samples_vs_q(ds_p_samples=self.ds_samples, log_p_samples=self.log_ps_samples)
+                kld = k.calculate_from_samples_vs_q(ds_p_samples=self.ds_samples, log_p_samples=self.log_ps_samples)
             row = [maf.layers, kld, jsd]
             values.append(row)
         values = np.array(values, dtype=np.float32)

maf/visual_random/EvalExample4.py
@@ -6,7 +6,7 @@ from typing import List
 from common.globals import Global
 from distributions.Distribution import Distribution
 from distributions.GaussianMultivariateFullCov import GaussianMultivariateFullCov
-from distributions.base import BaseMethods
+from distributions.base import BaseMethods, enable_memory_growth
 from maf.MaskedAutoregressiveFlow import MaskedAutoregressiveFlow
 from maf.visual_random.VisualRandomExample import VisualRandomExample
 
@@ -21,6 +21,7 @@ class EvalExample4(VisualRandomExample):
         rng = np.random.default_rng(45)
         sample_f = lambda: rng.normal(scale=2.0)
         cov = None
+        enable_memory_growth()
         for seed in range(1905, 10000):
             Global.set_seed(seed)
             # sample_f = lambda: np.random.normal(scale=2.0)
@@ -52,7 +53,7 @@ class EvalExample4(VisualRandomExample):
 if __name__ == '__main__':
 
     import tensorflow as tf
-
+    enable_memory_growth()
     for seed in range(1905, 10000):
         Global.set_seed(seed)
         sample_f = lambda: np.random.normal(scale=2.0)

visualise/CachePrinter.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -33,31 +35,34 @@ class CachePrinter:
         #     print('debug 4gerg')
 
         def printi(df: pd.DataFrame, ax, ax_log, print_log=False):
-            index = 0
-            if len(df) > 1:
-                index = 1
-            t: pd.DataFrame = df.iloc[index:]
-            max_y = t.values.max()
-            min_y = df.values.min()
-            print(f"limits {min_y} {max_y}")
-            ax.set_ylim(min_y, max_y)
-            ax.set_title(f"{', '.join(df.columns)}")
-            sns.lineplot(data=df, ax=ax)
-            if print_log:
-                df = df.copy()
-                for c in df.columns:
-                    df[c] = np.log(df[c])
-                # df['kl'] = np.log(df_kl['kl'])
-                # df['js'] = np.log(df_kl['js'])
-                # t: pd.DataFrame = df.loc[df.index >= 10]  # df.iloc[10:]
+            try:
+                index = 0
+                if len(df) > 1:
+                    index = 1
                 t: pd.DataFrame = df.iloc[index:]
                 max_y = t.values.max()
                 min_y = df.values.min()
-                ax_log.set_ylim(min_y, max_y)
-                ax_log.set_title(f"log {', '.join(df.columns)}")
-                sns.lineplot(data=df, ax=ax_log)
-            else:
-                ax_log.set_axis_off()
+                print(f"limits {min_y} {max_y}")
+                ax.set_ylim(min_y, max_y)
+                ax.set_title(f"{', '.join(df.columns)}")
+                sns.lineplot(data=df, ax=ax)
+                if print_log:
+                    df = df.copy()
+                    for c in df.columns:
+                        df[c] = np.log(df[c])
+                    # df['kl'] = np.log(df_kl['kl'])
+                    # df['js'] = np.log(df_kl['js'])
+                    # t: pd.DataFrame = df.loc[df.index >= 10]  # df.iloc[10:]
+                    t: pd.DataFrame = df.iloc[index:]
+                    max_y = t.values.max()
+                    min_y = df.values.min()
+                    ax_log.set_ylim(min_y, max_y)
+                    ax_log.set_title(f"log {', '.join(df.columns)}")
+                    sns.lineplot(data=df, ax=ax_log)
+                else:
+                    ax_log.set_axis_off()
+            except BaseException as e:
+                print(f"{e} in '{f}'", file=sys.stderr)
 
         print(f"printing '{f}'")
 
@@ -81,5 +86,5 @@ class CachePrinter:
 
 
 if __name__ == '__main__':
-    c = CachePrinter(Path('.cache'))
+    c = CachePrinter(Path('pull/.cache'))
     c.run()