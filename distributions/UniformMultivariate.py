d3d06be85ffcc5204e31c4faab986ab44d656134
xor <mrkeule@gmail.cpom>
2022-02-07 16:13:16 +0100

    use tf.Uniform

--- distributions/UniformMultivariate.py
+++ distributions/UniformMultivariate.py
@@ -5,8 +5,9 @@ from tensorflow_probability.python.distributions import Distribution as TD
 
 from common.NotProvided import NotProvided
 from distributions.Distribution import Distribution
-from distributions.Uniform import Uniform
 from distributions.base import TTensor, TTensorOpt, cast_to_ndarray
+from tensorflow_probability.python.distributions import Uniform as UU
+import tensorflow as tf
 
 
 class UniformMultivariate(Distribution):
@@ -14,25 +15,20 @@ class UniformMultivariate(Distribution):
         super().__init__(input_dim)
         self.lows: np.ndarray = self.cast_2_float_ndarray(lows)
         self.highs: np.ndarray = self.cast_2_float_ndarray(highs)
-        self.uniforms: List[Uniform] = None
 
     def _create_base_distribution(self) -> Optional[TD]:
-        self.uniforms: List[Uniform] = [Uniform(low=low, high=high) for low, high in zip(self.lows, self.highs)]
-        return None
+        return UU(self.lows, self.highs)
 
     def _log_likelihoods(self, xs: TTensor, cond: TTensorOpt = None) -> np.ndarray:
-        log_likelihoods = [u.log_likelihoods(xs[:, d]) for d, u in enumerate(self.uniforms)]
-        ls = np.sum(log_likelihoods, axis=0)
+        ls = self.tfd_distribution.log_prob(xs)
+        ls = tf.reduce_sum(ls, axis=1)
         return self.cast_2_likelihood(input_tensor=xs, result=ls)
 
     def _sample(self, size: int = 1, cond: TTensorOpt = None) -> np.ndarray:
-        samples = [u.sample(size) for u in self.uniforms]
-        result = np.column_stack(samples)
+        result = self.tfd_distribution.sample(sample_shape=(size,))
         return cast_to_ndarray(result)
 
     def _likelihoods(self, xs: TTensor, cond: TTensorOpt = None) -> np.ndarray:
-        likelihoods = [u.likelihoods(xs[:, d]) for d, u in enumerate(self.uniforms)]
-        ls = np.prod(likelihoods, axis=0)
+        ls = self.tfd_distribution.prob(xs)
+        ls = tf.math.reduce_prod(ls, axis=1)
         return self.cast_2_likelihood(input_tensor=xs, result=ls)
-
-
