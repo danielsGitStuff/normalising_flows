kl divergence more often, fix random cov matrix creation

distributions/GaussianMultivariateFullCov.py
@@ -4,13 +4,12 @@ import sys
 
 import numpy as np
 import tensorflow as tf
-from tensorflow_probability.python.distributions import MultivariateNormalFullCovariance, MultivariateNormalTriL
+from tensorflow_probability.python.distributions import MultivariateNormalTriL
 
 from common.NotProvided import NotProvided
 from distributions.Distribution import Distribution, TfpD
 from distributions.base import TTensor, TTensorOpt
 from typing import Optional
-from tensorflow_probability.python.distributions.distribution import Distribution as DD
 
 
 class GaussianMultivariateFullCov(Distribution):
@@ -43,7 +42,6 @@ class GaussianMultivariateFullCov(Distribution):
     def _sample(self, size: int = 1, cond: TTensorOpt = None, **kwargs) -> np.ndarray:
         samples = self.tfd_distribution.sample(sample_shape=(size,))
         return samples
-        return self.cast_2_float_ndarray(samples)
 
     def _likelihoods(self, xs: TTensor, cond: TTensorOpt = None) -> np.ndarray:
         return self.tfd_distribution.prob(xs)

distributions/base.py
@@ -4,6 +4,7 @@ import sys
 
 import math
 import setproctitle
+from sklearn.datasets import make_spd_matrix
 
 from common.globals import Global
 from typing import Union, Optional, Tuple, Dict, List, Callable
@@ -83,10 +84,8 @@ class BaseMethods:
         return m
 
     @staticmethod
-    def random_positive_semidefinite_matrix(n: int, sample_f: Callable[[], float], dtype=np.float32) -> np.ndarray:
-        m = BaseMethods.random_covariance_matrix(n=n, sample_f=sample_f, dtype=dtype)
-        c = np.dot(m, m.T)
-        return c
+    def random_positive_semidefinite_matrix(n: int, seed: int = 42) -> np.ndarray:
+        return make_spd_matrix(n_dim=n, random_state=seed)
 
     @staticmethod
     def un_nan(t: Tensor, replacement=0.0) -> Tensor: