d6665dcec179310a341ff7b45de3026695cf7fc7
xor <mrkeule@gmail.cpom>
2022-02-10 10:25:50 +0100

    refactoring

--- maf/dim10/zDim10bLargeGapTests.py
+++ maf/dim10/zDim10bLargeGapTests.py
@@ -4,11 +4,12 @@ from keta.argparseer import ArgParser
 from maf.dim10.EvalLargeD10 import EvalLargeD10
 
 
-class Dim10bLargeGaps(EvalLargeD10):
+class zDim10bLargeGapsTest(EvalLargeD10):
     def __init__(self, name: str = 'zDim10bLargeGapsTest', layers: Optional[List[int]] = None, loc_range: float = 10.0):
         super().__init__(name, layers=[1], loc_range=loc_range)
+        self.set_pool_size(2)
 
 
 if __name__ == '__main__':
     ArgParser.parse()
-    Dim10bLargeGaps().run()
+    zDim10bLargeGapsTest().run()
