34b879c084269c452757ae6be8ee8e0ee40bcdde
xor <mrkeule@gmail.cpom>
2022-02-10 10:31:56 +0100

    test

--- maf/dim10/ZDim10bLargeGapsTest.py
+++ maf/dim10/ZDim10bLargeGapsTest.py
@@ -7,7 +7,6 @@ from maf.dim10.EvalLargeD10 import EvalLargeD10
 class ZDim10bLargeGapsTest(EvalLargeD10):
     def __init__(self, name: str = 'zDim10bLargeGapsTest', layers: Optional[List[int]] = None, loc_range: float = 10.0):
         super().__init__(name, layers=[1], loc_range=loc_range)
-        self.set_pool_size(2)
 
 
 if __name__ == '__main__':
