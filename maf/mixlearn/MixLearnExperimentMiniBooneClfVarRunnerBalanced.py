bc4d5d94269a7033245120df7e0a9109bd7ad0af
xor <mrkeule@gmail.cpom>
2022-02-11 23:18:55 +0100

    faster

--- maf/mixlearn/MixLearnExperimentMiniBooneClfVarRunnerBalanced.py
+++ maf/mixlearn/MixLearnExperimentMiniBooneClfVarRunnerBalanced.py
@@ -5,7 +5,7 @@ from maf.mixlearn.dsinit.DSBalanceInitProcess import DSBalanceInitProcess
 
 class MixLearnExperimentMiniBooneClfVarRunnerBalanced(MixLearnExperimentMiniBooneClfVarRunner):
     def __init__(self):
-        super().__init__(name='miniboone_clf_var_balanced',pool_size=2)
+        super().__init__(name='miniboone_clf_var_balanced', pool_size=8)
         self.experiment_init_ds_class = DSBalanceInitProcess
 
 
