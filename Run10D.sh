c570541b9847810e3f45bc8116c3df64763ae311
xor <mrkeule@gmail.cpom>
2022-02-19 02:30:18 +0100

    test

--- Run10D.sh
+++ Run10D.sh
@@ -18,13 +18,14 @@ module="maf.dim10"
 #  python "bashlauncher.py" "$module.$klass" "$klass" "$gpu" &>"logs/$klass.log" &
 #done
 
+#gpu=0
+#python "bashlauncher.py" "$module.Dim10aCenteredMVG" "Dim10aCenteredMVG" "$gpu" &>"logs/Dim10aCenteredMVG.log" &
 gpu=0
-
-python "bashlauncher.py" "$module.Dim10aCenteredMVG" "Dim10aCenteredMVG" "$gpu" &>"logs/Dim10aCenteredMVG.log" &
-gpu=1
 nohup python "bashlauncher.py" "$module.Dim10bLargeGaps" "Dim10bLargeGaps" "$gpu" &>"logs/Dim10bLargeGaps.log" &
-gpu=2
+gpu=1
 nohup python "bashlauncher.py" "$module.Dim10cSmallGaps" "Dim10cSmallGaps" "$gpu" &>"logs/Dim10cSmallGaps.log" &
+gpu=2
+nohup python "bashlauncher.py" "$module.Dim10cSmallGaps" "Dim10dTinyGaps" "$gpu" &>"logs/Dim10dTinyGaps.log" &
 #python "bashlauncher.py" "$module.EvalExample6" "EvalExample6" "2" &>"logs/EvalExample6.log" &
 
 #nohup python "bashlauncher.py" "$module.EvalExample7" "EvalExample7" "1" &>"logs/EvalExample7.log" &
