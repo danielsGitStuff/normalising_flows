8b368971b0a4d7c78407ba55f6db9a52b47ef01d
xor <mrkeule@gmail.cpom>
2022-02-23 11:56:26 +0100

    update

--- RunMiniboone.sh
+++ RunMiniboone.sh
@@ -4,4 +4,15 @@ cd "$DIR" || exit
 
 . venv/bin/activate
 export PYTHONPATH=$(pwd)
-python maf/mixlearn/MixLearnExperimentMiniBooneRunDSizeVar.py
+
+mkdir -p "logs"
+gpu=0
+if [ -n "$1" ]; then
+  gpu=$1
+fi
+module="maf.mixlearn"
+
+gpu=0
+nohup python "bashlauncher.py" "$module.MixLearnExperimentMiniBooneClfVarRunner" "MixLearnExperimentMiniBooneClfVarRunner" "$gpu" &>"logs/MixLearnExperimentMiniBooneClfVarRunner.log" &
+gpu=1
+nohup python "bashlauncher.py" "$module.MixLearnExperimentMiniBooneClfVarRunnerBalanced" "MixLearnExperimentMiniBooneClfVarRunnerBalanced" "$gpu" &>"logs/MixLearnExperimentMiniBooneClfVarRunnerBalanced.log" &
