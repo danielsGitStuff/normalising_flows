54b89d8246367edc57cf306bc572fbad23e5f2b1
xor <mrkeule@gmail.cpom>
2022-02-23 12:08:06 +0100

    download miniboone dataset. prevents multiple processes downloading it simultaneously

---
+++ DownloadMiniBooNE.py
@@ -0,0 +1,10 @@
+from RunAllProcessWrapper import GPUProcessWrapper, GPUProcessWrapperPool
+from common.prozess.Prozessor import Prozessor
+from maf.mixlearn.dl3.MinibooneDL3 import MinibooneDL3
+
+if __name__ == '__main__':
+    # make sure the dataset is already in place before starting processes relying on it. Might cause race conditions otherwise
+    p : Prozessor = Prozessor()
+    dl3 = MinibooneDL3()
+    dl3.run()
+    print('donwloading miniboone done')
