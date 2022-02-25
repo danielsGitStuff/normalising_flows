cff10a1da32732d8aacb5d6534d7f69b81932465
xor <mrkeule@gmail.cpom>
2022-02-25 16:12:54 +0100

    name

--- maf/dim10/Dim10bSmallGaps.py
+++ maf/dim10/Dim10bSmallGaps.py
@@ -6,7 +6,7 @@ from maf.dim10.EvalLargeD10 import EvalLargeD10
 
 class Dim10bSmallGaps(EvalLargeD10):
 
-    def __init__(self, name: str = 'Dim10dTinyGaps', layers: Optional[List[int]] = None, loc_range: float = 2.2):
+    def __init__(self, name: str = 'Dim10bSmallGaps', layers: Optional[List[int]] = None, loc_range: float = 2.2):
         super().__init__(name, layers=layers, loc_range=loc_range)
 
 
